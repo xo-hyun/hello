9c2d0ec421cb9bd4254f76ef868c581d797bfefa
xo-hyun <xo_hyunn@naver.com>
2024-03-19 10:15:16 +0900

    setter, getter 추가[작업자 이름]

--- Person.java
+++ Person.java
@@ -1,6 +1,18 @@
 class Person{
 	String name;
 	int age;
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
 	public void sayHello(){
 		System.out.println("Hello," + name);
 	}
--- Rect.java
+++ Rect.java
@@ -1,6 +1,19 @@
 class Rect{
 	int width;
 	int height;
+	public void setWidth(int width){
+		this.width = width;
+	}
+	public void setHeight(){
+		this.height = height;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}
 	public int getArea(){
 		return width * height;
 	}
